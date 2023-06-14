647318c2f12bda5a9d7caf7dfd73e3e296412cf4
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-06-14 23:12:51 +0530

    first and last commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,157 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  const style = {};
+
+  return(
+    <header className="header" >
+       <h1 style={style} >
+        Fast React Pizza Co.
+        </h1>
+    </header>
+  )
+  
+ 
+}
+function Menu() {
+  const pizzas = pizzaData;
+  const pizzaLength = pizzas.length;
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      
+      {pizzaLength> 0 ? (
+        <>
+        <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+        <ul className="pizzas">
+        { pizzaData.map((pizza) => {
+          return(
+            <Pizza
+            pizza={pizza}
+            />
+          )
+        }) }
+        </ul>
+        </>
+          
+      ): (<p>We're still working on our menu, Please come back later!</p>)}
+    </main>
+  );
+}
+
+function Pizza({pizza}) {
+  const {name , ingredients, photoName, soldOut, price} = pizza;
+
+
+  return (
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
+      <img src={photoName} alt={name} />
+      <h3>{name}</h3>
+      <p>{ingredients}</p>
+      {/* {soldOut ? (
+        <span>SOLD OUT</span>
+      ): (
+        <span>{price}</span>
+      )} */}
+      <span>{soldOut ? "SOLD OUT" : price}</span>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = (hour >= openHour && hour <= closeHour);
+
+ return (
+    <footer className="footer">
+        {isOpen ? (
+         <Order
+         closeHour = {closeHour}
+         />
+        ): (<p>We are happy to welcome you between {openHour}:00 and {closeHour}:00</p>)}
+       
+    </footer>
+    // React.createElement('footer' , null , "We're currently open")
+  );
+}
+
+function Order(props){
+  const {closeHour} = props;
+  return(
+    <div className="order">
+    <p>
+      We're currently open untill {closeHour}:00. Come visit us or order online.
+    </p>
+    <button className="btn">Order</button>
+  </div>
+  )
+}
+
+
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
